feaef0e42851ab964604f7d324b09b8dbb360963
Salo Bagdadi <salomon.bagdadi@c3ntro.com>
2022-06-24 11:04:53 -0500

    commit inicial

---
+++ src/views/CounterView.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>
+       Counter: {{ counterStore.counter }} 
+    </h1>
+    <button @click="counterStore.incrementBy(1)">mas 1</button>
+    <button @click="counterStore.incrementBy(2)">mas 2</button>
+    <button @click="counterStore.incrementBy(5)">mas 5</button>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>
+       Home: {{ counterStore.counter }}
+    </h1>
+    <h3>
+       Cambios: {{ counterStore.numberOfChanges }}
+    </h3>
+</template>
+
+<script setup lang="ts">
+
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
+
+<style scoped>
+
+</style>
